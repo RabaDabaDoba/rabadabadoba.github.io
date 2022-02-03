5424d9fe611ffd1e5c8519397ac74fe115cdd959
rabadabadoba <robindah@kth.se>
2022-02-03 12:24:42 +0100

    update

---
+++ script/03-02-2022-12-23-21-This-is-the-first-scripted-post.md
@@ -0,0 +1,5 @@
+---
+title: This is the first scripted post
+date: 03-02-2022-12-23-21
+layout: post
+---
--- script/genPost.sh
+++ script/genPost.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-read title
+echo "Title of the page:";
+read title;
+echo "Layout: ";
+read layout;
 
-DATE=$(date +%d-%m-%Y"-"%H-%M-%S);
+DATEL=$(date +%d-%m-%Y"-"%H-%M-%S);
+DATEP=DATEL=$(date +%d-%m-%Y"-"%H-%M-%S);
+TITLESTRING=$(echo "$title" | tr " " -);
+FINAL=$DATEL"-"$TITLESTRING".md";
 
-echo $DATE;
+
+
+touch $FINAL;
+echo "---" >> $FINAL;
+echo "title: "$title >> $FINAL;
+echo "date: "$DATEL >> $FINAL;
+echo "layout: "$layout >> $FINAL;
+echo "---" >> $FINAL;
+echo $FINAL;
+#---
+#title: "name does not matter"
+#layout: post
+#date: 1997-06-12 11:04 +0200
+#category: test
+#---
